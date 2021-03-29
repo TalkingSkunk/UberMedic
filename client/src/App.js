c3c6d96bfa910be38c395e5bc7638d6988c0718b
Sam Kim <sendtosamkim@gmail.com>
2021-03-29 17:02:10 -0400

    erased openlayers

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,14 @@ import GeoJSON from 'ol/format/GeoJSON';
 import { Controls, FullScreenControl } from "./Controls";
 import Navbar from "./Navbar/Navbar"
 import CallDetails from "./CallDetails/CallDetails"
-import socketIOClient from "socket.io-client";
+import { io } from "socket.io-client";
 
 // make socket connection from the frontend
-const socket = socketIOClient('http://localhost:3000');
+const PORT = 8080;
 
-
+const socket = io(`http://localhost:${PORT}`, {
+     transports: ['websocket', 'polling', 'flashsocket']
+});
 
 document.addEventListener('DOMContentLoaded', ()=>{
     if (!navigator.geolocation){
@@ -22,32 +24,20 @@ document.addEventListener('DOMContentLoaded', ()=>{
     }
     // 1. success fxn called with location info 2. error catch fxn
     navigator.geolocation.watchPosition((position)=>{
-		//emit brand new event unrgistered yet
-		
+        //emit brand new event unrgistered yet
         socket.emit('newLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
             timestamp: position.timestamp,
         })
-		console.log('position:', position)
-        
-        
     }, (error)=>{
         alert('unable to fetch location; permission denied :(')
     })
 })
 
-let styles = {
-	'Point': new Style({
-		image: new CircleStyle({
-			radius: 10,
-			fill: null,
-			stroke: new Stroke({
-				color: 'magenta',
-			}),
-		}),
-	}),
-};
+
+
+
 
 
 
@@ -55,18 +45,12 @@ let styles = {
 
 
 const App = () => {
-	
-	const [center, setCenter] = useState([-79.51039453904934, 43.64672462788966]);
-	const [zoom, setZoom] = useState(11.5);
-	const [showLayer1, setShowLayer1] = useState(true);
-	const [showLayer2, setShowLayer2] = useState(true);
 
 	useEffect(()=>{
 		//listen for events
-		socket.on('returnLocation', (coords)=>{
-
-		console.log(` add cords ${coords.longitude}, ${coords.latitude}`)
-		setCenter(center = [coords.longitude, coords.latitude])
+		socket.on('returnLocation', data=>{
+			console.log('your position is:', data)
+			// setCenter(center = [data.longitude, data.latitude])
 		})
 	}, [])
 
@@ -75,18 +59,6 @@ const App = () => {
 		<div>
         	<Navbar />
 				<CallDetails />
-
-				<Map center={fromLonLat(center)} zoom={zoom}>
-					<Layers>
-						<TileLayer
-							source={osm()}
-							zIndex={0}
-						/>
-					</Layers>
-					<Controls>
-						<FullScreenControl />
-					</Controls>
-				</Map>
 		</div>	
 	);
 }
