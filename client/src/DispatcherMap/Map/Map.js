5c372b2589478521e8519acae85fb944e36c34e6
Sam Kim <sendtosamkim@gmail.com>
2021-03-31 13:19:20 -0400

    map to map socket

--- client/src/DispatcherMap/Map/Map.js
+++ client/src/DispatcherMap/Map/Map.js
@@ -10,25 +10,68 @@ mapboxgl.workerClass = MapboxWorker;
 mapboxgl.accessToken = 'pk.eyJ1IjoidGFsa2luZ3NrdW5rIiwiYSI6ImNrbXYyYTAyNDAwejMydm52aThnZ3BvY3kifQ.ER8YYxoj5YJD_-8m1hNdxg';
 
 
+
+import openSocket from 'socket.io-client';
+const socket = openSocket('http://localhost:8080');
+
+
+
+let marker
+let lng = -79.4718
+let lat = 43.6708
+
 // This defines Map then specifies that it should be rendered in the <div> with the ID of app.
 const Map = () => {
+    
     //The state stores the longitude, latitude, and zoom for the map. These values will all change as your user interacts with the map.
     const mapContainer = useRef();
-    const [lng, setLng] = useState(-79.4718);
-    const [lat, setLat] = useState(43.6708);
+    // const [lng, setLng] = useState(-79.4718);
+    // const [lat, setLat] = useState(43.6708);
     const [zoom, setZoom] = useState(11.5);
-// The Mapbox map is initialized within React's Effect hook or the componentDidMount() lifecycle method, if you are using classes. Initializing your map here ensures that Mapbox GL JS will not try to render a map before React creates the element that contains the map. You also set the following options inside the map initialization: (
-    // The container option tells Mapbox GL JS to render the map inside a specific DOM element. Here, the app expects to receive a mapContainer ref. Later in this tutorial, you will assign the ref that is being referenced here to an HTML element that will act as the map container.
-    // The style option defines the style that the map will use (mapbox://styles/mapbox/streets-v11).
+
+
+    
+    
+
+    
+    // The Mapbox map is initialized within React's Effect hook or the componentDidMount() lifecycle method, if you are using classes. Initializing your map here ensures that Mapbox GL JS will not try to render a map before React creates the element that contains the map. You also set the following options inside the map initialization: (
+        // The container option tells Mapbox GL JS to render the map inside a specific DOM element. Here, the app expects to receive a mapContainer ref. Later in this tutorial, you will assign the ref that is being referenced here to an HTML element that will act as the map container.
+        // The style option defines the style that the map will use (mapbox://styles/mapbox/streets-v11).
     // The center and zoom options set the center coordinates and zoom level of the map using the values of lng, lat, and zoom that are stored in state.)
 useEffect(() => {
+    
+
     const map = new mapboxgl.Map({
         container: mapContainer.current,
         style: 'mapbox://styles/mapbox/streets-v11',
         center: [lng, lat],
         zoom: zoom
     });
+    marker = new mapboxgl.Marker({
+        color: "#000066",
+        draggable: false,
+        }).setLngLat([lng, lat])
+        .addTo(map)
     map.on('load', () => {
+
+        socket.on('receiveMedicPosition', data => {
+            lng = data.longitude;
+            lat = data.latitude;
+            if (marker) {
+                marker.setLngLat([lng,lat])
+            }
+            map.flyTo({
+                center: [lng,lat],
+                zoom: 13
+            });
+            // setLng(data.longitude)
+            // setLat(data.latitude)
+            console.log('receiving from server', data)
+          });
+        // window.setInterval(function () {
+        //     console.log( `adjusing the coordsz; ${lat}, ${lng}` )
+        //     marker.setLngLat([lng, lat]).addTo(map)
+        //  }, 2000 )
         // Add navigation control (+/- top right, and directions on top left)
         map.addControl(new mapboxgl.NavigationControl(), 'top-right');
 
@@ -39,22 +82,9 @@ useEffect(() => {
             }), 'bottom-left'
         );
 
-        if (!navigator.geolocation){
-            return alert('Geolocation not supported by your browser :(')
-        }
-        navigator.geolocation.watchPosition(position => {
-            const userCoordinates = [parseFloat(position.coords.longitude.toFixed(5)), parseFloat(position.coords.latitude.toFixed(5))];
-            console.log('your location:', userCoordinates)
-            var marker = new mapboxgl.Marker({
-                color: "#FFFFFF",
-                draggable: false,
-                }).setLngLat(userCoordinates)
-                .addTo(map)
-            map.flyTo({
-                center: userCoordinates,
-                zoom: 13
-            });
-        }, err=>{console.log(err)}, {enableHighAccuracy: true});
+
+
+
     })
 
 
@@ -65,8 +95,8 @@ useEffect(() => {
     
     // // you need to create a function that stores the new latitude, longitude, and zoom that you get when a user interacts with the map. You will write a Mapbox GL JS map.on('move') function that sets the state to these new values when a user moves the map.
     map.on('move', () => {
-        setLng(map.getCenter().lng.toFixed(4));
-        setLat(map.getCenter().lat.toFixed(4));
+        lng = (map.getCenter().lng.toFixed(4));
+        lat = (map.getCenter().lat.toFixed(4));
         setZoom(map.getZoom().toFixed(2));
     });
  
@@ -74,7 +104,7 @@ useEffect(() => {
 
      // Clean up on unmount
      return () => map.remove();
-    }, []);
+}, []);
 
   // The mapContainer ref specifies that map should be drawn to the HTML page in a new <div> element.
   return (
--- server.js
+++ server.js
@@ -3,18 +3,26 @@ require( 'dotenv' ).config() // looks for .env ; process.env gets it's values
 const path = require('path')
 const express = require('express')
 const app = express()
-var cors = require('cors')
-app.use(cors())
+// var cors = require('cors')
+// app.use(cors())
+
+const server = require("http").createServer();
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 
 const PORT = process.env.PORT || 8080
 
 
-//require socket library
-const socketio = require('socket.io')
 
-const saveCoords = require ('./app/db/orm.mobileUnitRoutes')
 
+
+// for parsing incoming POST data
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 
 if( !process.env.MONGODB_URI ){
@@ -22,43 +30,49 @@ if( !process.env.MONGODB_URI ){
    process.exit()
 }
 
-// for parsing incoming POST data
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
 
 
-const server = app.listen(PORT, ()=>{
-   console.log(`listening to requests on PORT: ${PORT}`)
-})
-
-// socket set-up : invoke the function socket (parameter) >> waiting now for browser to connect to server
-
-
-const io = socketio( server )
+console.log ('yoyoma')
 // listen for 'connection' event between browser and server >> callback fxn; pass the instance of the socket(object for each browser) 'socket' as parameter
   
-io.on('connection', (client)=>{
-   
-   console.log('user connected')
-   socket.on('newLocationMessage', async (coords)=>{
-      // to all sockets
-      console.log('receiving messsage from react')
-      io.emit ('returnLocation', coords)
-      let data = {
-         callID: 112,
-         unitNumber: 2021,
-         CTAS: 'ECHO',
-         CC: 'chest pain',
-         location: '1234 sunnyside dr',
-         intersection: 'bathurst and bloor',
-         police: 'on scene',
-         fire: 'on scene',
-         medic: 'on scene',
-         coords: coords,
-      }
-      console.log(`longitude and latitude: ${coords.longitude},${coords.latitude} at ${coords.timestamp}`)
-      send this thisloc to mongo
-      await saveCoords(data);
+io.on('connection', (socket)=>{
 
+   console.log('user connected')
+
+   socket.on('lol', msg=>console.log(msg))
+   
+   socket.on('medicPosition', data=>{
+      console.log(`receiving coordinate from medicside: lng ${data.longitude}, lat ${data.latitude} at time ${data.timestamp}`)
+      io.emit('receiveMedicPosition', data)
    })
+   
+   // socket.on('newLocationMessage', async (coords)=>{
+   //    // to all sockets
+   //    console.log('receiving messsage from react')
+   //    io.emit ('returnLocation', coords)
+   //    let data = {
+   //       callID: 112,
+   //       unitNumber: 2021,
+   //       CTAS: 'ECHO',
+   //       CC: 'chest pain',
+   //       location: '1234 sunnyside dr',
+   //       intersection: 'bathurst and bloor',
+   //       police: 'on scene',
+   //       fire: 'on scene',
+   //       medic: 'on scene',
+   //       coords: coords,
+   //    }
+
+
+   socket.on ('disconnect', reason => {
+      console.log('user has disconnected :(')
+   })
+
+
+})
+
+
+
+server.listen(PORT, () => {
+   console.log (`listening on *:${PORT}`)
 })
