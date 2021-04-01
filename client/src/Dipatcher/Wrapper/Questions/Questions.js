63cb1677de16ba08e81db49a31efff12a9e4f124
Vithushan <[valid-email]>
2021-04-01 09:59:11 -0400

    updating questions

--- client/src/Dipatcher/Wrapper/Questions/Questions.js
+++ client/src/Dipatcher/Wrapper/Questions/Questions.js
@@ -7,6 +7,7 @@ function Questions () {
         return (
             <>
             <ul>
+        {/* QUESTION 1 */}
                  <h1>1.) Are you able to wake the patient?</h1>
                  <InputGroup className="mb-3">
     <InputGroup.Prepend>
@@ -29,13 +30,76 @@ function Questions () {
   <InputGroup className="mb-3">
     <InputGroup.Prepend>
     <div>
-    <label>  Breathing?</label>
+    <label> Comments</label>
     </div>
-      <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+   
     </InputGroup.Prepend>
     <FormControl aria-label="Text input with checkbox" />
   </InputGroup> 
 
+
+  {/* QUESTION 2 */}
+
+  <h1>2.) Is the Patient breathing? </h1>
+                 <InputGroup className="mb-3">
+    <InputGroup.Prepend>
+    <div>
+    <label>  YES</label>
+    </div>
+   
+      <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+
+    
+    <div>
+    <label>  NO</label>
+    </div>
+      <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+    </InputGroup.Prepend>
+    
+    
+  </InputGroup>
+
+  <InputGroup className="mb-3">
+    <InputGroup.Prepend>
+    <div>
+    <label> Comments</label>
+    </div>
+   
+    </InputGroup.Prepend>
+    <FormControl aria-label="Text input with checkbox" />
+  </InputGroup> 
+
+    {/* QUESTION 3 */}
+
+
+    <h1>3.) Is the patient bleeding? </h1>
+                 <InputGroup className="mb-3">
+    <InputGroup.Prepend>
+    <div>
+    <label>  YES</label>
+    </div>
+   
+      <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+
+    
+    <div>
+    <label>  NO</label>
+    </div>
+      <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+    </InputGroup.Prepend>
+    
+    
+  </InputGroup>
+
+  <InputGroup className="mb-3">
+    <InputGroup.Prepend>
+    <div>
+    <label> Comments</label>
+    </div>
+   
+    </InputGroup.Prepend>
+    <FormControl aria-label="Text input with checkbox" />
+  </InputGroup> 
    <InputGroup className="mb-3">
     <InputGroup.Prepend>
     <div>
