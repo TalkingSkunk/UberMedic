d637a9caca15f9ba05dffec7f5f87ad625505b51
Vithushan <[valid-email]>
2021-04-03 15:21:13 -0400

    added changes

--- client/src/Dipatcher/Wrapper/Questions/Questions.js
+++ client/src/Dipatcher/Wrapper/Questions/Questions.js
@@ -8,6 +8,8 @@ import Col from 'react-bootstrap/Col';
 import CardDeck from 'react-bootstrap/CardDeck';
 import Card from 'react-bootstrap/Card';
 import ModalInFunctionalComponent from '../../Wrapper/Questions/modal2/modal2'
+import Form from 'react-bootstrap/Form'
+import './Questions.css'
 
 
 function Questions () {
@@ -15,8 +17,9 @@ function Questions () {
         return (
             <>
             <ul>
+              <h1>Screening Questions</h1>
         {/* QUESTION 1 */}
-                 <h1>1.) Is the patient conscious?</h1>
+                 <h2>1.) Is the patient conscious?</h2>
                  <InputGroup className="mb-3">
     <InputGroup.Prepend>
     <div>
@@ -35,20 +38,11 @@ function Questions () {
     
   </InputGroup>
 
-  <InputGroup className="mb-3">
-    <InputGroup.Prepend>
-    <div>
-    <label> Comments</label>
-    </div>
-   
-    </InputGroup.Prepend>
-    <FormControl aria-label="Text input with checkbox" />
-  </InputGroup> 
-
+ 
 
   {/* QUESTION 2 */}
 
-  <h1>2.) Is the Patient breathing? </h1>
+  <h2>2.) Is the Patient breathing? </h2>
                  <InputGroup className="mb-3">
     <InputGroup.Prepend>
     <div>
@@ -64,20 +58,12 @@ function Questions () {
     
   </InputGroup>
 
-  <InputGroup className="mb-3">
-    <InputGroup.Prepend>
-    <div>
-    <label> Comments</label>
-    </div>
-   
-    </InputGroup.Prepend>
-    <FormControl aria-label="Text input with checkbox" />
-  </InputGroup> 
+  
 
     {/* QUESTION 3 */}
 
 
-    <h1>3.) Is the patient bleeding? </h1>
+    <h2>3.) Is the patient bleeding? </h2>
                  <InputGroup className="mb-3">
     <InputGroup.Prepend>
     <div>
@@ -96,58 +82,8 @@ function Questions () {
     
   </InputGroup>
 
-  <InputGroup className="mb-3">
-    <InputGroup.Prepend>
-    <div>
-    <label> Comments</label>
-    </div>
-   
-    </InputGroup.Prepend>
-    <FormControl aria-label="Text input with checkbox" />
-  </InputGroup> 
-            </ul>     
-
-{/* <div >
-<Button variant= 'primary'> Alpha </Button>
-<Button variant= 'info'> Bravo </Button>
-<Button variant= 'btn btn-success'> Charlie </Button>
-<Button variant= 'btn btn-warning'> Delta  </Button>
-<Button variant= 'btn btn-danger'> Echo </Button>
-
-</div> */}
-
-{/* <Container>
-  <Row>
-    <Col sm={8}><strong> Select Approriate Scenario</strong> </Col>
-    <Col sm={4}> <strong> LEGEND </strong></Col>
-  </Row>
-  <Row>
-    <Col sm><Button variant= 'primary'> Alpha </Button></Col>
-
-    <Col sm><Button variant= 'primary'> Alpha </Button> Non Serious or non life threatening</Col>
-  </Row>
-  <Row>
-  <Col sm><Button variant= 'info'> Bravo </Button></Col>
-
-  <Col sm><Button variant= 'info'> Bravo </Button>    Serious not life threatening – urgent</Col>
-  </Row>
-  <Row>
-  <Col sm><Button variant= 'btn btn-success'> Charlie </Button></Col>
-
-  <Col sm><Button variant= 'btn btn-success'> Charlie </Button>   Serious not life threatening – immediate</Col>
-  </Row>
-  <Row>
-  <Col sm><Button variant= 'btn btn-warning'> Delta  </Button></Col>
   
-  <Col sm><Button variant= 'btn btn-warning'> Delta  </Button> Life threatening other than cardiac or respiratory arrest</Col>
-  </Row>
-  <Row>
-  <Col sm><Button variant= 'btn btn-danger'> Echo </Button></Col>
-
-  <Col sm><Button variant= 'btn btn-danger'> Echo </Button>         Life threatening – Cardiac or respiratory arrest</Col>
-  </Row>
-
-</Container> */}
+            </ul>     
 
 
 <CardDeck >
@@ -166,40 +102,46 @@ function Questions () {
         </div>
       </Card.Body>
      
-      <ModalInFunctionalComponent/>
+      <Col sm> <ModalInFunctionalComponent/></Col>
       <Card.Footer className="text-muted"><Button variant="primary">Submit</Button></Card.Footer>
     </Card>
 
 
-{/* NEAREST AMBULANCE */}
-    {/* <Card className="text-center" >
-      <Card.Header>LEGEND</Card.Header>
-      <Card.Body >
-        <Row>
-        <Col sm><Button variant= 'primary'> Alpha </Button> Non Serious or non life threatening</Col>
-        </Row>
-        <Row>
-        <Col sm><Button variant= 'info'> Bravo </Button>Serious not life threatening – urgent</Col>
-        </Row>
-        <Row>
-        <Col sm><Button variant= 'btn btn-success'> Charlie </Button> Serious not life threatening – immediate</Col>
-        </Row>
-        <Row>
-        <Col sm><Button variant= 'btn btn-warning'> Delta  </Button> Life threatening other than cardiac or respiratory arrest</Col>
-        </Row>
-        <Row>
-        <Col sm><Button variant= 'btn btn-danger'> Echo </Button>  Life threatening – Cardiac or respiratory arrest</Col>
-        </Row>
-      */}
-     {/* <Button variant= 'primary'> Alpha </Button> Non Serious or non life threatening
-     <Button variant= 'info'> Bravo </Button>Serious not life threatening – urgent
-     <Button variant= 'btn btn-success'> Charlie </Button> Serious not life threatening 
-     <Button variant= 'btn btn-warning'> Delta  </Button> Life threatening other than cardiac or respiratory arrest
-     <Button variant= 'btn btn-danger'> Echo </Button>  Life threatening – Cardiac or respiratory arrest */}
 
-      {/* </Card.Body>
-      <Card.Footer className="text-muted">Submitted/not submitted</Card.Footer>
-    </Card> */}
+
+
+    
+<Card className="text-center" >
+      <Card.Header>CHIEF COMPLIANT</Card.Header>
+      <Card.Body>
+        <Card.Title >IMPORTANT DETAILS ONLY <i class="fas fa-info-circle"></i></Card.Title>
+    
+    <div>
+    <Form.Group controlId="exampleForm.ControlTextarea1">
+    <Form.Control as="textarea" rows={3} />
+  </Form.Group>
+        </div>
+      </Card.Body>
+
+      <Card.Footer className="text-muted"><Button variant="primary">Submit</Button></Card.Footer>
+    </Card>
+
+
+
+    
+<Card className="text-center" >
+      <Card.Header>NOTES</Card.Header>
+      <Card.Body>
+      <Card.Title >ADDITIONAL DETAILS TO NOTE </Card.Title>
+      <div>
+    <Form.Group controlId="exampleForm.ControlTextarea1">
+    <Form.Control as="textarea" rows={3} />
+  </Form.Group>
+        </div>
+      </Card.Body>
+     
+      <Card.Footer className="text-muted"><Button variant="primary">Submit</Button></Card.Footer>
+    </Card>
 
 </CardDeck>
             </>
