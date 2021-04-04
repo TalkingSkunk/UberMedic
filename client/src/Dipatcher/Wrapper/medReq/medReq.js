5decdab3ac8927d31e95cc2a5843f3c99e2c666f
Sam Kim <sendtosamkim@gmail.com>
2021-04-03 20:50:26 -0400

    struggle with deleting request

--- client/src/Dipatcher/Wrapper/medReq/medReq.js
+++ client/src/Dipatcher/Wrapper/medReq/medReq.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react'
+import React, {useContext, useState} from 'react'
 import {MedicDispatchContext} from "../../../utils/MedicDispatchContext";
 import Modal from "react-bootstrap/Modal";
 import ListGroup from "react-bootstrap/ListGroup";
@@ -10,24 +10,47 @@ const MedReq = () =>{
     const {medRequest} = useContext(MedicDispatchContext)
     const [ medReqOut, setMedReqOut ] = medRequest
 
+    const [reqContent, setReqContent] = useState("")
 
-    const handleApprove = ()=>{
+    const handleApprove = (e)=>{
         // clear request after approve from the list
-        
+        // setMedReqOut(prevReq => [...prevReq, {id: dataOut.id, for: dataOut.for}])
+        // const revisedComments = state.comments.filter( (_,idx)=>idx!==action.value )
+        const isId = e.target.dataset.id
+        const isFor = e.target.dataset.for
+        setMedReqOut(prevReq=>{
+            prevReq.filter(item=>item.id !== isId && item.for !== isFor )
+        })
+        setShow(false);
     }
 
     // modals stuff clicks
     const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const [modalId, setModalId] = useState("")
+    const [modalFor, setModalFor] = useState("")
+    
+    const handleClose = (e) => {
+        const isId = e.target.dataset.id
+        const isFor = e.target.dataset.for
+        setMedReqOut(prevReq=>{
+            prevReq.filter(item=>item.id !== isId && item.for !== isFor )
+        })
+        setShow(false);
+    }
+    const handleShow = (e) => {
+        setReqContent(e.target.innerText)
+        setModalId(e.target.dataset.id)
+        setModalFor(e.target.dataset.for)
+        setShow(true);
+    }
 
     return(
         <ListGroup>
             {medReqOut.map(data=>{
                 return (
                     <>
-                        <ListGroup.Item action onClick={handleShow}>
-                            [{data.id}] Requesting {data.for}...
+                        <ListGroup.Item action onClick={handleShow} data-id={data.id} data-for={data.for}>
+                            [{data.id}] Requesting >> {data.for}...
                         </ListGroup.Item>
                         <Modal
                             show={show}
@@ -36,16 +59,18 @@ const MedReq = () =>{
                             keyboard={false}
                         >
                             <Modal.Header closeButton>
-                            <Modal.Title>Modal title</Modal.Title>
+                            <Modal.Title>Resource Request</Modal.Title>
                             </Modal.Header>
                             <Modal.Body>
+                                {reqContent}
+                                <br/>
                                 Please Contact Ambulance Prior to Decision.
                             </Modal.Body>
                             <Modal.Footer>
-                            <Button variant="secondary" onClick={handleClose}>
+                            <Button variant="secondary" onClick={handleClose} data-id={modalId} data-for={modalFor}>
                                 Disregard
                             </Button>
-                            <Button variant="primary" onClick={handleApprove}>Approve</Button>
+                            <Button variant="primary" onClick={handleApprove} data-id={modalId} data-for={modalFor}>Approve</Button>
                             </Modal.Footer>
                         </Modal>
                     </>
