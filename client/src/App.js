29972d2c892c6b44652b64589b0a211ba15db435
Giovanni <lugk4l@gmail.com>
2021-04-06 14:27:41 -0400

    dark theme in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,41 @@
 import React, { useState } from "react";
-import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import DispatcherWrapper from "./Dipatcher/Wrapper";
 import MedicWrapper from "./Medic/Wrapper/Wrapper";
 import Login from "./loginPage";
 import { MedicDispatchProvider } from "./utils/MedicDispatchContext";
+import { ThemeProvider } from "styled-components";
+import { useDarkMode } from "./DarkTheme/useDarkMode";
+import { GlobalStyles } from "./DarkTheme/GlobalStyles";
+import { lightTheme, darkTheme } from "./DarkTheme/Themes";
+import Toggle from "./DarkTheme/Toggler";
+// import "./App.css";
 
 const App = () => {
+  const [theme, themeToggler, mountedComponent] = useDarkMode();
+
+  const themeMode = theme === "light" ? lightTheme : darkTheme;
   /////stateful
+  if (!mountedComponent) return <div />;
+
   return (
-    <div>
-      <MedicDispatchProvider>
-        <BrowserRouter>
-          <Route path="/dispatcher" exact component={DispatcherWrapper} />
+    <ThemeProvider theme={themeMode}>
+      <>
+        <GlobalStyles />
+        <Toggle theme={theme} toggleTheme={themeToggler} />
+        <MedicDispatchProvider>
+          <BrowserRouter>
+            <Route path="/dispatcher" exact component={DispatcherWrapper} />
 
-          <Route path="/medical" exact component={MedicWrapper} />
+            <Route path="/medical" exact component={MedicWrapper} />
 
-          <Route path="/" exact component={Login} />
+            <Route path="/" exact component={Login} />
 
-          {/* <Route path="*" exact component={} /> */}
-        </BrowserRouter>
-      </MedicDispatchProvider>
-    </div>
+            {/* <Route path="*" exact component={} /> */}
+          </BrowserRouter>
+        </MedicDispatchProvider>
+      </>
+    </ThemeProvider>
   );
 };
 
