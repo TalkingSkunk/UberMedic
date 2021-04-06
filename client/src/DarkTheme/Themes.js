06d0a36c6988a7a23bf8528760175e4c70777118
Giovanni <lugk4l@gmail.com>
2021-04-06 14:43:11 -0400

    dask theme implemented

--- client/src/DarkTheme/Themes.js
+++ client/src/DarkTheme/Themes.js
@@ -2,11 +2,11 @@ export const lightTheme = {
   body: "#FFF",
   text: "#363537",
   toggleBorder: "#FFF",
-  backgroundColor: "#363537",
+  background: "#363537",
 };
 export const darkTheme = {
   body: "#363537",
   text: "#FAFAFA",
   toggleBorder: "#6B8096",
-  backgroundColor: "#999",
+  background: "#999",
 };
